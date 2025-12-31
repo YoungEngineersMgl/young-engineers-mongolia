73db9d9beb67dc5b96d62dbb1dc387f7582aa372
Sarangerel624 <sarangerelbatbold11@gmail.com>
2025-12-31 16:39:19 +0800

    i

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,15 @@ import { Home, PenSquare, CalendarPlus, Menu, X } from "lucide-react";
 
 import { useAdminAuth } from "@/providers/adminAuth";
 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Card, CardContent } from "@/components/ui/card";
+
 type NavItemProps = {
   icon: React.ElementType;
 
@@ -179,30 +188,28 @@ const Page = () => {
           )}
         </header>
 
-        <div className="flex justify-around">
-          <div className="text-7xl font-bold flex flex-col  justify-items-start align-items h-[100%] gap-2 text-white bit-white-glow">
+        <div className="flex justify-around pt-50">
+          <div className="text-8xl font-bold flex flex-col  justify-items-start align-items h-[100%] gap-2 text-white bit-white-glow mt-20">
             <div>Young </div>
-
             <div>Engineers</div>
-
             <div>Mongolia</div>
           </div>
 
           <div>
             <img
               src="/test.png"
-              className="w-[500px] h-[400px] rounded-2xl shadow-lg"
+              className="w-[600px] h-[500px] rounded-2xl shadow-lg"
             />
           </div>
         </div>
 
-        <div className="flex mt-34 justify-around">
+        <div className="flex mt-34 justify-around pb-30">
           <div>
-            <div className="text-4xl font-bold text-white bit-white-glow">
+            <div className="text-6xl font-bold text-white bit-white-glow">
               Young Engineers Mongolia
             </div>
 
-            <div className="text-[20px] text-white w-[600px]  mt-3">
+            <div className="text-[23px] text-white w-[600px]  mt-6">
               <span className="text-blue-200 text-glow-blue">
                 Young Engineers Mongolia
               </span>{" "}
@@ -226,18 +233,102 @@ const Page = () => {
 
       <section>
         <div>
-          <img src="/bg.png" className="w-full h-auto object-cover" />
+          <img src="/bg.png" className="w-full h-auto absolute" />
 
-          <div>
-            <div className="text-2xl">Төслүүд</div>
+          <div className="flex flex-col gap-10  relative">
+            <div className="rounded-2xl bg-gray-900/80 backdrop-blur-md  m-20 p-10 shadow-lg mt-50 pb-20">
+              <div className="text-4xl text-white font-bold bit-white-glow">
+                Төслүүд
+              </div>
 
-            <div>
-              <div>Young Engineers Mongolia</div>
+              <div className="flex gap-4 mt-10">
+                <div className=" flex flex-col gap-10 text-white text-lg bg-blue-950 rounded-lg p-6 border-2 border-amber-50 shadow-blue-200/50 shadow-lg">
+                  <div className="text-2xl bit-white-glow">
+                    Young Engineers Mongolia
+                  </div>
+                  <div className="-mt-5 text-[23px]">
+                    <span className="backdrop-invert-05 text-blue-200 bit-white-glow">
+                      Young Engineers Academy
+                    </span>{" "}
+                    төсөл нь одоогоор манай байгууллагын хамгийн том төсөл
+                    бөгөөд нийт 50 гаруй ажиллах хүчний багтайгаар хэрэгжиж
+                    байна. Энэхүү төсөл нь "Е" буюу 9 инженерчлэлийн салбарыг
+                    нийтэд таниулан суртачилах гол зорилготой бөгөөд уг зорилгын
+                    хүрээнд 1 том хэмжээний эвент, 2 workshop, 72 блог, 51 цахим
+                    контент тус тус хийсэн билээ.
+                  </div>
+                </div>
+                <div className="flex flex-col gap-10 text-white text-lg bg-blue-950  rounded-lg p-6  border-2 border-amber-50 shadow-blue-200/50 shadow-lg">
+                  <div className="text-2xl bit-white-glow">
+                    Young Engineers Program
+                  </div>
 
-              <div></div>
+                  <div className="-mt-5 text-[23px]">
+                    <span className="backdrop-invert-05 text-blue-200 bit-white-glow">
+                      Young Engineers Program{" "}
+                    </span>
+                    төсөл нь 7 хоног бүр салбар бүрийн хүндэт инженерүүдийг
+                    зочин илтгэгчээр урин, оролцогчиддоо чанартай боловсролыг
+                    олгосон. Мөн оролцогчдыг 3 баг болгон хувааж, нийгэмд
+                    нөлөөлсөн багийн төслүүдийг нь хөгжүүлсэн ба оролцогчид
+                    маань хөтөлбөрөөс сурсан зүйлсээ хуваалцах нийт 3 удаагийн
+                    сургалтуудыг ЕБС-ийн сурагчдад орсон.
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+      </section>
+      <section className="min-h-screen w-full bg-[url('/pro3.jpg')] bg-cover bg-center">
+        <div className="flex flex-col gap-10 pt-30 p-10">
+          <div className="text-4xl text-white bit-white-glow font-bold">
+            Блог
+          </div>
+          <div className="text-[23px] text-white scroll -mt-5">
+            Бид 9E-ээр буюу 9 инженерчлэлийн чиглэлийн хүрээнд залуучуудад
+            чанартай бөгөөд уншихад хялбар блогуудыг бичиж оруулдаг. 7 хоног бүр
+            тогтмол хуваарьтайгаар блогууд маань орж байгаа тул та бүхэн өдөр
+            болгон вебсайтаа шалгаж байгаарай!
+          </div>
+        </div>
+         {" "}
+        <Carousel
+          opts={{
+            align: "start",
+          }}
+          className="w-full max-w-sm"
+        >
+              {" "}
+          <CarouselContent>
+                  {" "}
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                          {" "}
+                <div className="p-1">
+                              {" "}
+                  <Card>
+                                  {" "}
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                                      {" "}
+                      <span className="text-3xl font-semibold">
+                        {index + 1}
+                      </span>
+                                    {" "}
+                    </CardContent>
+                                {" "}
+                  </Card>
+                            {" "}
+                </div>
+                        {" "}
+              </CarouselItem>
+            ))}
+                {" "}
+          </CarouselContent>
+               <CarouselPrevious />
+               <CarouselNext />
+            {" "}
+        </Carousel>
       </section>
     </div>
   );

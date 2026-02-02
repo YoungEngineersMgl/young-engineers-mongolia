aa19c074f59f0377e3aac84d223a6eba9cd9ec54
Sarangerel624 <sarangerelbatbold11@gmail.com>
2026-02-02 22:59:51 +0800

    a

--- lib/email.ts
+++ lib/email.ts
@@ -10,20 +10,18 @@ export async function sendInviteEmail(
   const inviteLink = `${process.env.APP_URL}/activate/${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "Young Engineers <no-reply@youngengineersmongolia.com>",
     to: email,
     subject: `Admin эрхийн урилга – Аккаунтаа идэвхжүүлнэ үү.`,
-    html: `  <p>Сайн байна уу,</p>
+    html: `
+      <p>Сайн байна уу,</p>
       <p>Танд Young Engineers Mongolia байгууллагын ${role} админ эрхийг олгохоор урьж байна.</p>
       <p>Доорх холбоосоор орж аккаунтаа идэвхжүүлснээр админ эрх тань хүчин төгөлдөр болно.</p>
-      <p>
-        Аккаунт идэвхжүүлэх холбоос:
-        ${inviteLink}
-      </p>
+      <p>Аккаунт идэвхжүүлэх холбоос: <a href="${inviteLink}">${inviteLink}</a></p>
       <h3>Анхаарна уу:</h3>
       <p>Энэхүү холбоос нь 24 цагийн дараа автоматаар хүчингүй болох тул хугацаанд нь идэвхжүүлнэ үү.</p>
       <p>Хүндэтгэсэн,</p>
-     <p> Young Engineers Mongolia</p>
-      `,
+      <p>Young Engineers Mongolia</p>
+    `,
   });
 }
